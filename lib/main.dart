ecca23f104f51218a5bac862a325b168feb28e1b
sajibsaha4647 <sksaha4648@gmail.com>
2022-07-15 14:53:15 +0600

    add appbar

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,34 @@ void main() {
 
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
+
+
+
   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
 
-class _MyAppState extends State<MyApp> {
+
   @override
   Widget build(BuildContext context) {
+    var size,height,width;
+    // getting the size of the window
+    // size = MediaQuery.of(context).size;
+    // height = size.height;
+    // width = size.width;
+
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Deshboard",style: TextStyle(fontSize: 20)),
+        leading: Icon(Icons.tv_off),
+        actions: [
+          IconButton(onPressed: (){print("sajib press");}, icon: Icon(Icons.camera)),
+          IconButton(onPressed: null, icon: Icon(Icons.phone))
+          ],
+        ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children:  [
